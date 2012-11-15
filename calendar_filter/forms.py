695192e4ef07fbfe6e52e9b72c430dac0145953e
Viorel Stirbu <viorels@gmail.com>
2012-11-15 10:46:29 +0200

    bootstrap layout and better presentation

--- calendar_filter/forms.py
+++ calendar_filter/forms.py
@@ -7,8 +7,13 @@ EVENT_STATUS_CHOICES = (('accepted', 'Accepted'),
                       ('needs-action', 'Needs Action'))
 
 class CalendarForm(forms.Form):
-    url = forms.CharField(label="Facebook events link")
+    url = forms.CharField(
+        label="Facebook events link",
+        widget=TextInput(attrs={
+            "size": "40",
+            "placeholder": "webcal://www.facebook.com/ical/u.php?uid=..."})
+        )
     status = forms.MultipleChoiceField(
         choices=EVENT_STATUS_CHOICES,
         required=True,
-        label="Accepted statuses")
+        label="Allowed statuses")
